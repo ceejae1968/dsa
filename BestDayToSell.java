cbb431ad33d7e04c934acfacc59dd0b4a51c2dfc
ceejae1968 <43509231+ceejae1968@users.noreply.github.com>
2026-07-13 12:02:20 +0530

    Best Day to Sell

--- BestDayToSell.java
+++ BestDayToSell.java
@@ -1,1 +1,21 @@
+package algos;
+
+public class BestDayToSell {
+    public static void main(String[] args){
+        int[] prices = {7,6,5,4,3,2};
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;                 // best day to buy so far
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);  // best sell today
+            }
+        }
+
+        System.out.println(maxProfit);
+    }
+}
 
